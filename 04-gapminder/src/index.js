5ba45b8a682b5e3027f814ab9491129dcb8aa5fa
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-03-24 10:23:29 +0100

    Ajout des fichiers de l'exercice 4

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,8 +1,11 @@
-import * as d3 from 'd3'
+// import * as d3 from "../node_modules/d3";
+
+document.querySelector("body").textContent = "Coucou";
 
 // Pour importer les données
-// import file from '../data/data.csv'
+import populationData from "../data/population_total.csv";
 
+// Récupère toutes les années
+const annees = Object.keys(populationData[0]);
 
-
-
+console.log(annees);
